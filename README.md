8a9a0d6a8c8ddc3e5315205ef3497d8fcd1453b3
frederikstroem <frederikstroem@pm.me>
2019-08-26 20:55:52 +0200

    Initial release

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sober October Countdown
+## Third-party software used
+- [Chris Kempson's Tomorrow Night color scheme](https://github.com/chriskempson/base16-tomorrow-scheme/blob/master/tomorrow-night.yaml)
---
+++ js/master.js
@@ -0,0 +1,41 @@
+// Constansts.
+const currentLocalDate = new Date();
+
+// DOM elements.
+var mainText = document.getElementById("mainText");
+
+// Set first header element in mainText.
+mainText.querySelector(":nth-child(1)").innerHTML = "There is";
+
+// Check weather it is october.
+if (currentLocalDate.getMonth() == 10 - 1) {  // -1 because of starting months starting at 0.
+  // It is october.
+  // Time to 31th of october.
+  var countdownTo = new Date(currentLocalDate.getFullYear(), 10 - 1, 31);
+  mainText.querySelector(":nth-child(2)").innerHTML = Math.floor(
+    (countdownTo.getTime() - currentLocalDate.getTime()) / (1000 * 60 * 60 * 24) + 1
+  );
+  mainText.querySelector(":nth-child(2)").innerHTML += " day";
+  if (currentLocalDate.getDate() != 30) {
+    mainText.querySelector(":nth-child(2)").innerHTML += "s";
+  }
+  mainText.querySelector(":nth-child(3)").innerHTML = "to the last day of October.";
+} else {
+  // It is not october.
+  // Time to 1st of october.
+  var countdownToYear = currentLocalDate.getFullYear();
+  if ((currentLocalDate.getMonth() + 1) > 10) {
+    // After October.
+    countdownToYear++;
+  }
+  var countdownTo = new Date(countdownToYear, 10 - 1, 1, 0, 0, 0);
+
+  var daysLeft = Math.floor((countdownTo.getTime() - currentLocalDate.getTime()) / (1000 * 60 * 60 * 24)) + 1;
+
+  mainText.querySelector(":nth-child(2)").innerHTML = daysLeft;
+  mainText.querySelector(":nth-child(2)").innerHTML += " day";
+  if (daysLeft != 1) {
+    mainText.querySelector(":nth-child(2)").innerHTML += "s";
+  }
+  mainText.querySelector(":nth-child(3)").innerHTML = "to the first day of October.";
+}
